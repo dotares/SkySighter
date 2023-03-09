eb97eb6ec8c73edd14a5c53e696f3ea4765b9b56
Amaari <arish2k3@gmail.com>
2023-03-09 11:21:18 +0530

    add init code

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+// GEOCODE API: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+//
+// CURRENT WEATHER DATA: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+// API KEYS:
+
+const openWeatherAPIKey = config.OPEN_WEATHER_API_KEY;
