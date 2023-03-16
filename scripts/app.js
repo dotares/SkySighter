3754269cff2c2c15e1df67cf3cc438da0fa44eb1
AMAARIDOR <arish2k3@gmail.com>
2023-03-16 12:10:26 +0530

    add location integration with html

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ submitButton.addEventListener("click", () => {
 
   (async () => {
     // call the geocoding function
-    const { latitude, longitude } = await getLatitudeAndLongitude("New York");
+    const { latitude, longitude } = await getLatitudeAndLongitude(userLocation);
 
     // call the main functions
     const dataWeather = await getWeatherData(latitude, longitude, userUnit);
@@ -19,33 +19,29 @@ submitButton.addEventListener("click", () => {
       userUnit
     );
 
-    // TODO:
-    // 2. Figure out the open weather map feature
-    // 3. Design basic structure with tailwind
-    // 4. Set up variables in tailwind structure html
-
     const currentDay = new Date(dataWeather.dt * 1000);
-    const day = currentDay.toLocaleDateString("en-US");
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
 
-    // dataWeather stuff
-    const dataWeatherMain = Object.entries(dataWeather.main);
-    const dataWeatherWeatherStatus = Object.entries(dataWeather.weather[0]);
-    const dataWeatherSunState = Object.entries(dataWeather.sys);
-    const dataWeatherWind = Object.entries(dataWeather.wind);
+    console.log(currentDay.toLocaleDateString("en-US", options));
 
-    // dataWeather stuff without objects or arrays
-    const dataWeatherTimezone = dataWeather.timezone;
-    const dataWeatherVisibility = dataWeather.visibility;
+    // TODO:
+    // 1. Get data and attach to html
 
-    // dataAP stuff
-    const dataAPComponents = Object.entries(dataAP.list[0].components);
-    const dataAPMain = Object.entries(dataAP.list[0].main);
+    // Quick info container temperature according to user's unit
+    userUnit === "metric"
+      ? (tempTxt.textContent = `${dataWeather.main.temp.toFixed(0)}℃`)
+      : (tempTxt.textContent = `${dataWeather.main.temp.toFixed(0)}℉`);
 
-    console.log(dataWeatherMain);
-    console.log(dataWeatherWeatherStatus);
-    console.log(dataWeatherSunState);
-    console.log(dataWeatherTimezone);
-    console.log(dataWeatherVisibility);
-    console.log(dataWeatherWind);
+    // Quick info container weather status
+    statusTxt.textContent = `${dataWeather.weather[0].main}`;
+
+    // Quick info container location
+    locationTxt.innerHTML = `<i class="fa-solid fa-location-dot m-2"></i> ${userLocation}, ${dataWeather.sys.country}`;
+
+    // Quick info container date
   })();
 });
