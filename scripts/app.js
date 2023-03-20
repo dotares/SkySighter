5a6308aa94a06c63ec8077b1164aa0de55827544
Amaari <arish2k3@gmail.com>
2023-03-20 11:36:18 +0530

    update

--- scripts/app.js
+++ scripts/app.js
@@ -21,166 +21,172 @@ const map = L.map("map");
 
 // Submit button event handler and call the functions to find weather data according to the user data
 submitButton.addEventListener("click", () => {
-  const userLocation = locationInput.value;
-  const userUnit = unitsInput.value;
+	const userLocation = locationInput.value;
+	const userUnit = unitsInput.value;
 
-  // function call async
+	// function call async
 
-  (async () => {
-    // call the geocoding function
-    const { latitude, longitude } = await getLatitudeAndLongitude(userLocation);
+	(async () => {
+		// call the geocoding function
+		const { latitude, longitude } = await getLatitudeAndLongitude(
+			userLocation
+		);
 
-    // call the main functions
-    const dataWeather = await getWeatherData(latitude, longitude, userUnit);
-    const dataAP = await getAirPollutionData(latitude, longitude, userUnit);
-    const dataForecast = await get5DayForecastData(
-      latitude,
-      longitude,
-      userUnit
-    );
+		// call the main functions
+		const dataWeather = await getWeatherData(latitude, longitude, userUnit);
+		const dataAP = await getAirPollutionData(latitude, longitude, userUnit);
+		const dataForecast = await get5DayForecastData(
+			latitude,
+			longitude,
+			userUnit
+		);
 
-    const currentDay = new Date(dataWeather.dt * 1000);
+		console.log(dataWeather);
 
-    const options = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
+		const currentDay = new Date(dataWeather.dt * 1000);
 
-    const currentTime = `${String(currentDay.getHours()).padStart(
-      2,
-      "0"
-    )}:${String(currentDay.getMinutes()).padStart(2, "0")}`;
+		const options = {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		};
 
-    // Quick info temperature status
-    if (userUnit === "standard")
-      tempTxt.textContent = `${dataWeather.main.temp.toFixed(1)} K`;
-    if (userUnit === "metric")
-      tempTxt.textContent = `${dataWeather.main.temp.toFixed(1)}℃`;
-    if (userUnit === "imperial")
-      tempTxt.textContent = `${dataWeather.main.temp.toFixed(1)}℉`;
+		const currentTime = `${String(currentDay.getHours()).padStart(
+			2,
+			"0"
+		)}:${String(currentDay.getMinutes()).padStart(2, "0")}`;
 
-    // Quick info container weather status
-    statusTxt.textContent = `${dataWeather.weather[0].main}`;
+		// Quick info temperature status
+		if (userUnit === "standard")
+			tempTxt.textContent = `${dataWeather.main.temp.toFixed(1)} K`;
+		if (userUnit === "metric")
+			tempTxt.textContent = `${dataWeather.main.temp.toFixed(1)}℃`;
+		if (userUnit === "imperial")
+			tempTxt.textContent = `${dataWeather.main.temp.toFixed(1)}℉`;
 
-    // Quick info container location
-    locationTxt.innerHTML = `<i class="fa-solid fa-location-dot m-2"></i> ${userLocation}, ${dataWeather.sys.country}`;
+		// Quick info container weather status
+		statusTxt.textContent = `${dataWeather.weather[0].main}`;
 
-    // Quick info container date
-    dateTimeTxt.innerHTML = `<i class="fa-solid fa-calendar-days m-2"></i> ${currentDay.toLocaleDateString(
-      "en-US",
-      options
-    )}`;
+		// Quick info container location
+		locationTxt.innerHTML = `<i class="fa-solid fa-location-dot m-2"></i> ${userLocation}, ${dataWeather.sys.country}`;
 
-    // Quick info time
-    timeTxt.innerHTML = `<i class="fa-solid fa-clock m-2"></i> ${currentTime}`;
+		// Quick info container date
+		dateTimeTxt.innerHTML = `<i class="fa-solid fa-calendar-days m-2"></i> ${currentDay.toLocaleDateString(
+			"en-US",
+			options
+		)}`;
 
-    // Secondary info winds
-    if (userUnit === "imperial")
-      windsTxt.textContent = `${dataWeather.wind.speed} mph winds`;
-    else windsTxt.textContent = `${dataWeather.wind.speed} m/s winds`;
+		// Quick info time
+		timeTxt.innerHTML = `<i class="fa-solid fa-clock m-2"></i> ${currentTime}`;
 
-    // Secondary info humidity
-    humidityTxt.textContent = `${dataWeather.main.humidity}% humidity`;
+		// Secondary info winds
+		if (userUnit === "imperial")
+			windsTxt.textContent = `${dataWeather.wind.speed} mph winds`;
+		else windsTxt.textContent = `${dataWeather.wind.speed} m/s winds`;
 
-    // Secondary info temp min and max
-    if (userUnit === "standard")
-      minAndMaxTempTxt.textContent = `${dataWeather.main.temp_min.toFixed(
-        0
-      )} K min/${dataWeather.main.temp_max.toFixed(0)} K max`;
-    if (userUnit === "metric")
-      minAndMaxTempTxt.textContent = `${dataWeather.main.temp_min.toFixed(
-        0
-      )}℃ min/${dataWeather.main.temp_max.toFixed(0)}℃ max`;
-    if (userUnit === "imperial")
-      minAndMaxTempTxt.textContent = `${dataWeather.main.temp_min.toFixed(
-        0
-      )}℉ min/${dataWeather.main.temp_max.toFixed(0)}℉ max`;
+		// Secondary info humidity
+		humidityTxt.textContent = `${dataWeather.main.humidity}% humidity`;
 
-    // Secondary info visibility
-    if (userUnit === "imperial")
-      visibilityTxt.textContent = `${(
-        dataWeather.visibility * 0.000621371
-      ).toFixed(1)} miles visibility`;
-    else
-      visibilityTxt.textContent = `${
-        dataWeather.visibility / 1000
-      } km visibility`;
+		// Secondary info temp min and max
+		if (userUnit === "standard")
+			minAndMaxTempTxt.textContent = `${dataWeather.main.temp_min.toFixed(
+				0
+			)} K min/${dataWeather.main.temp_max.toFixed(0)} K max`;
+		if (userUnit === "metric")
+			minAndMaxTempTxt.textContent = `${dataWeather.main.temp_min.toFixed(
+				0
+			)}℃ min/${dataWeather.main.temp_max.toFixed(0)}℃ max`;
+		if (userUnit === "imperial")
+			minAndMaxTempTxt.textContent = `${dataWeather.main.temp_min.toFixed(
+				0
+			)}℉ min/${dataWeather.main.temp_max.toFixed(0)}℉ max`;
 
-    // Secondary info sunrise to sunset
-    const sunrise = new Date(dataWeather.sys.sunrise * 1000);
-    const sunset = new Date(dataWeather.sys.sunset * 1000);
+		// Secondary info visibility
+		if (userUnit === "imperial")
+			visibilityTxt.textContent = `${(
+				dataWeather.visibility * 0.000621371
+			).toFixed(1)} miles visibility`;
+		else
+			visibilityTxt.textContent = `${
+				dataWeather.visibility / 1000
+			} km visibility`;
 
-    const sunriseFormatted = `${String(sunrise.getHours()).padStart(
-      2,
-      "0"
-    )}:${String(sunrise.getMinutes()).padStart(2, "0")}`;
+		// Secondary info sunrise to sunset
+		const sunrise = new Date(dataWeather.sys.sunrise * 1000);
+		const sunset = new Date(dataWeather.sys.sunset * 1000);
 
-    const sunsetFormatted = `${String(sunset.getHours()).padStart(
-      2,
-      "0"
-    )}:${String(sunset.getMinutes()).padStart(2, "0")}`;
+		const sunriseFormatted = `${String(sunrise.getHours()).padStart(
+			2,
+			"0"
+		)}:${String(sunrise.getMinutes()).padStart(2, "0")}`;
 
-    sunriseAndSunsetTxt.innerHTML = `From ${sunriseFormatted} <br> to ${sunsetFormatted}`;
+		const sunsetFormatted = `${String(sunset.getHours()).padStart(
+			2,
+			"0"
+		)}:${String(sunset.getMinutes()).padStart(2, "0")}`;
 
-    // Secondary info feels like temperature
-    if (userUnit === "standard")
-      feelsLikeTempTxt.textContent = `Feels like, ${dataWeather.main.feels_like.toFixed(
-        0
-      )} K`;
-    if (userUnit === "metric")
-      feelsLikeTempTxt.textContent = `Feels like, ${dataWeather.main.feels_like.toFixed(
-        0
-      )}℃`;
-    if (userUnit === "imperial")
-      feelsLikeTempTxt.textContent = `Feels like, ${dataWeather.main.feels_like.toFixed(
-        0
-      )}℉`;
+		sunriseAndSunsetTxt.innerHTML = `From ${sunriseFormatted} <br> to ${sunsetFormatted}`;
 
-    // Map setup for changing the view of the map according to the location
-    map.setView([latitude, longitude], 13);
+		// Secondary info feels like temperature
+		if (userUnit === "standard")
+			feelsLikeTempTxt.textContent = `Feels like, ${dataWeather.main.feels_like.toFixed(
+				0
+			)} K`;
+		if (userUnit === "metric")
+			feelsLikeTempTxt.textContent = `Feels like, ${dataWeather.main.feels_like.toFixed(
+				0
+			)}℃`;
+		if (userUnit === "imperial")
+			feelsLikeTempTxt.textContent = `Feels like, ${dataWeather.main.feels_like.toFixed(
+				0
+			)}℉`;
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-    }).addTo(map);
+		// Map setup for changing the view of the map according to the location
+		map.setView([latitude, longitude], 13);
 
-    // filter out the timestamps without the 00 hour unit
-    const [...timestamps] = dataForecast.list;
+		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			maxZoom: 19,
+		}).addTo(map);
 
-    const result = timestamps.filter((step) =>
-      step.dt_txt.includes("00:00:00")
-    );
+		// filter out the timestamps without the 00 hour unit
+		const [...timestamps] = dataForecast.list;
 
-    const days = [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ];
+		const result = timestamps.filter((step) =>
+			step.dt_txt.includes("00:00:00")
+		);
 
-    let finalHtml = ``;
+		const days = [
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+			"Sunday",
+		];
 
-    // add html in the forecast box of the weather application
-    for (let day of result) {
-      const date = new Date(day.dt * 1000);
-      const dayNumber = date.getDay();
-      const html = `
+		let finalHtml = ``;
+
+		// add html in the forecast box of the weather application
+		for (let day of result) {
+			const date = new Date(day.dt * 1000);
+			const dayNumber = date.getDay();
+			const html = `
             <div class="day flex flex-row p-5">
               <div class="px-2">icon</div>
-              <div class="px-2 forecast_min_and_max_temp">${day.main.temp_max}${
-        userUnit === "metric" ? "°C" : "°F"
-      }/${day.main.temp_min}${userUnit === "metric" ? "°C" : "°F"}</div>
+              <div class="px-2 forecast_min_and_max_temp">${
+					userUnit === "metric"
+						? `${day.main.temp}°C`
+						: `${day.main.temp}°F`
+				}</div>
               <div class="px-2 forecast_date">${day.dt_txt.split(" ")[0]}</div>
               <div class="px-2 forecast_day">${days[dayNumber - 1]}</div>
             </div>
       `;
 
-      finalHtml += html;
-    }
+			finalHtml += html;
+		}
 
-    insertTxt.innerHTML = finalHtml;
-  })();
+		insertTxt.innerHTML = finalHtml;
+	})();
 });
