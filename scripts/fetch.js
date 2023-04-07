c5cf23377a3c969d7c18d5d43d74b1e0857e11e3
Amaari <arish2k3@gmail.com>
2023-04-07 18:53:24 +0530

    final

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -5,77 +5,30 @@ const openWeatherAPIKey = config.OPEN_WEATHER_API_KEY;
 // Function for geocoding
 
 const getLatitudeAndLongitude = async (location) => {
-  try {
-    const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${openWeatherAPIKey}`
-    );
-
-    if (!response.ok)
-      throw new Error(`${response.statusText}: (${response.status})`);
-
-    const data = await response.json();
-
-    const { lat: latitude, lon: longitude } = await data[0];
-
-    return { latitude, longitude };
-  } catch (err) {
-    console.log(err);
-  }
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${openWeatherAPIKey}`
+  );
+  if (!response.ok)
+    throw new Error(`${response.statusText}: (${response.status})`);
+  const [data] = await response.json();
+  return { latitude: data.lat, longitude: data.lon };
 };
 
 // Function for getting all types of API endpoint data
 
-const getWeatherData = async (lat, lon, unit) => {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=${unit}&lon=${lon}&appid=${openWeatherAPIKey}`
-    );
-
-    if (!response.ok)
-      throw new Error(`${response.statusText}: (${response.status})`);
-
-    const data = await response.json();
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+const fetchData = async function (endpoint) {
+  const response = await fetch(endpoint);
+  if (!response.ok)
+    throw new Error(`${response.statusText}: (${response.status})`);
+  return response.json();
 };
 
-// Function for getting air pollution data
-
-const getAirPollutionData = async (lat, lon, unit) => {
-  try {
-    const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&units=${unit}&lon=${lon}&appid=${openWeatherAPIKey}`
-    );
-
-    if (!response.ok)
-      throw new Error(`${response.statusText}: (${response.status})`);
-
-    const data = await response.json();
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+const getWeatherData = async function (lat, lon, unit) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=${unit}&lon=${lon}&appid=${openWeatherAPIKey}`;
+  return fetchData(url);
 };
 
-// Function for getting 5 day forcast with 3 hour steps
-
-const get5DayForecastData = async (lat, lon, unit) => {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&units=${unit}&lon=${lon}&appid=${openWeatherAPIKey}`
-    );
-
-    if (!response.ok)
-      throw new Error(`${response.statusText}: (${response.status})`);
-
-    const data = await response.json();
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+const get5DayForecastData = async function (lat, lon, unit) {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&units=${unit}&lon=${lon}&appid=${openWeatherAPIKey}`;
+  return fetchData(url);
 };
